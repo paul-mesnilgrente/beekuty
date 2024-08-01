ce65d5edec8b843ae3d7f8269aa7e4fb32cd40b5
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-08-01 23:18:31 +0200

    style: rubocop fixes

--- scripts/update_gallery_data.rb
+++ scripts/update_gallery_data.rb
@@ -11,25 +11,27 @@ require 'date'
 unless ENV['INSTAGRAM_ACCESS_TOKEN']
   params = [
     "client_id=#{ENV['INSTAGRAM_APP_ID']}",
-    "redirect_uri=https://beekuty.fr/auth",
-    "scope=user_profile,user_media",
-    "response_type=code"
+    'redirect_uri=https://beekuty.fr/auth',
+    'scope=user_profile,user_media',
+    'response_type=code'
   ]
   Launchy.open("https://api.instagram.com/oauth/authorize?#{params.join('&')}")
 
   print 'Please enter the code (excluding #): '
   code = gets.chomp
 
-  codesResponse = Faraday.post('https://api.instagram.com/oauth/access_token', {
-    client_id: ENV['INSTAGRAM_APP_ID'],
-    client_secret: ENV['INSTAGRAM_APP_SECRET'],
-    grant_type: 'authorization_code',
-    redirect_uri: 'https://beekuty.fr/auth',
-    code: code
-  })
+  codes_response = Faraday.post(
+    'https://api.instagram.com/oauth/access_token', {
+      client_id: ENV['INSTAGRAM_APP_ID'],
+      client_secret: ENV['INSTAGRAM_APP_SECRET'],
+      grant_type: 'authorization_code',
+      redirect_uri: 'https://beekuty.fr/auth',
+      code: code
+    }
+  )
 
   # Exchanging the code against a token:
-  response_json = JSON.parse(codesResponse.body)
+  response_json = JSON.parse(codes_response.body)
   unless response_json['access_token']
     puts "ERROR when getting the access_token with #{ENV['INSTAGRAM_AUTH_CODE']}"
     puts response_json
@@ -40,25 +42,28 @@ unless ENV['INSTAGRAM_ACCESS_TOKEN']
   puts 'Please run:'
   puts "export INSTAGRAM_ACCESS_TOKEN=#{response_json['access_token']}"
   puts "export INSTAGRAM_USER_ID=#{response_json['user_id']}"
-  puts "And run the script again"
+  puts 'And run the script again'
 
   exit 0
 end
 
 # get the media
-mediasResponse = Faraday.get('https://graph.instagram.com/me/media', {
-  fields: 'id,children,caption,media_type,media_url,timestamp',
-  access_token: ENV['INSTAGRAM_ACCESS_TOKEN']
-})
+medias_response = Faraday.get(
+  'https://graph.instagram.com/me/media', {
+    fields: 'id,children,caption,media_type,media_url,timestamp',
+    access_token: ENV['INSTAGRAM_ACCESS_TOKEN']
+  }
+)
 
-allMedias = JSON.parse(mediasResponse.body)
+all_medias = JSON.parse(medias_response.body)
 
 ################
 # ADD PAGINATION
 ################
 data = []
-allMedias['data'].each do |media|
+all_medias['data'].each do |media|
   next unless media['media_type'] == 'IMAGE'
+
   if media['media_type'] == 'IMAGE'
     data << {
       'id' => media['id'],
@@ -68,7 +73,7 @@ allMedias['data'].each do |media|
     }
     response = Faraday.get(media['media_url'])
     if response.success?
-      if !File.exists?("assets/images/nailarts/#{media['id']}.jpg")
+      unless File.exist?("assets/images/nailarts/#{media['id']}.jpg")
         File.open("assets/images/nailarts/#{media['id']}.jpg", 'wb') { |f| f.write(response.body) }
       end
     else
@@ -77,11 +82,13 @@ allMedias['data'].each do |media|
   elsif media['media_type'] == 'CAROUSEL_ALBUM'
     children = []
     media['children']['data'].each do |child|
-      childResponse = Faraday.get("https://graph.instagram.com/#{child['id']}", {
-        fields: 'media_url',
-        access_token: ENV['INSTAGRAM_ACCESS_TOKEN']
-      })
-      children << JSON.parse(childResponse.body)['media_url']
+      child_response = Faraday.get(
+        "https://graph.instagram.com/#{child['id']}", {
+          fields: 'media_url',
+          access_token: ENV['INSTAGRAM_ACCESS_TOKEN']
+        }
+      )
+      children << JSON.parse(child_response.body)['media_url']
     end
     data << {
       'type' => 'album',
