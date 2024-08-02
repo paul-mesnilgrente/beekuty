8da48edede42d0d98429bd944763517a8cef3a93
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-08-02 11:21:56 +0200

    feat: added request logging and refactored requests

--- scripts/update_gallery_data.rb
+++ scripts/update_gallery_data.rb
@@ -7,48 +7,62 @@ require 'pry'
 require 'launchy'
 require 'date'
 
-class RequestException
-  def initialize(response, exception)
-    @response = response
-    @exception = exception
+# Instagram module
+module Instgrm
+  # Used to stop the script when failing and log debugging logs
+  class RequestException
+    def initialize(exception)
+      @exception = exception
+    end
+
+    def call
+      binding.pry
+      puts 'Request failed'
+      exit 1
+    end
   end
 
-  def call
-    binding.pry
-    puts 'Request failed'
-    exit 1
+  # Create requests for instagram
+  class Request
+    def initialize
+      @connection = Faraday.new(url: 'https://graph.instagram.com/') do |faraday|
+        faraday.request :json
+        faraday.response :json
+        faraday.response :logger
+        faraday.response :raise_error
+      end
+      @access_token_param = { access_token: ENV['INSTAGRAM_ACCESS_TOKEN'] }
+    end
+
+    def fetch_image(media_url)
+      Faraday.new(media_url) { |faraday| faraday.response :raise_error }.get.body
+    rescue Faraday::Error => e
+      RequestException.new(e).call
+    end
+
+    def get(path, params)
+      @connection.get(path, params.merge(@access_token_param)).body
+    rescue Faraday::Error => e
+      RequestException.new(e).call
+    end
   end
 end
 
 def first_medias
-  medias_response = Faraday.get(
-    'https://graph.instagram.com/me/media', {
-      fields: 'id,children,caption,media_type,media_url,timestamp',
-      access_token: ENV['INSTAGRAM_ACCESS_TOKEN']
-    }
+  Instgrm::Request.new.get(
+    '/me/media', { fields: 'id,children,caption,media_type,media_url,timestamp' }
   )
-  JSON.parse(medias_response.body)
 end
 
 def download_image(media)
-  response = Faraday.get(media['media_url'])
-  if response.success?
-    unless File.exist?("assets/images/nailarts/#{media['id']}.jpg")
-      File.open("assets/images/nailarts/#{media['id']}.jpg", 'wb') { |f| f.write(response.body) }
-    end
-  else
-    puts "Failed to download #{media['id']}"
-  end
+  response = Instgrm::Request.new.fetch_image(media['media_url'])
+  return if File.exist?("assets/images/nailarts/#{media['id']}.jpg")
+
+  File.open("assets/images/nailarts/#{media['id']}.jpg", 'wb') { |f| f.write(response.body) }
 end
 
 def fetch_child(id)
-  response = Faraday.get(
-    "https://graph.instagram.com/#{id}", {
-      fields: 'media_url',
-      access_token: ENV['INSTAGRAM_ACCESS_TOKEN']
-    }
-  )
-  JSON.parse(response.body)
+  Instgrm::Request.new.get("/#{id}", { fields: 'media_url' })
 end
 
 def download_children(media)
@@ -84,13 +98,8 @@ def download_media(media)
 end
 
 # get the media
-
-medias = first_medias
-
-################
-# ADD PAGINATION
-################
 data = []
+medias = first_medias
 medias['data'].each do |media|
   data << download_media(media)
 end
