94238c2c04db91261770f2dddf8d5578d19234df
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-11-14 16:07:22 +0100

    feat: removed the modal for name change

--- assets/js/modal_on_load.js
+++ assets/js/modal_on_load.js
@@ -1,13 +1,16 @@
+const SHOW_MODAL = false;
+const MODAL_NAME = 'name_update';
+
 window.addEventListener('load', function() {
   let modal_on_load_value = localStorage.getItem("modal_on_load")
   const modalElement = document.getElementById('modal-on-load');
 
-  if (modal_on_load_value !== 'seen') {
+  if (SHOW_MODAL && modal_on_load_value !== MODAL_NAME) {
     const bsModal = new bootstrap.Modal(modalElement)
     bsModal.show()
   }
 
   modalElement.addEventListener('hide.bs.modal', function() {
-    localStorage.setItem("modal_on_load", "seen");
+    localStorage.setItem("modal_on_load", MODAL_NAME);
   })
 })
